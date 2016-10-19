cb735e75458d29ff7eee3f9b2b926372a98b49bf
somename <someemail>
2016-10-19 14:43:25 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -9,10 +9,22 @@ public class TestPlanetExplorer {
 		
 		assertEquals("(5,5)", explorer.getSize());
 	}
+	
 	@Test
-	public void test_createPlanet_5x5() {
-		PlanetExplorer explorer = new PlanetExplorer(5, 5, null);
+	public void test_createPlanet_100x100() {
+		PlanetExplorer explorer = new PlanetExplorer(100, 100, null);
 		
-		assertEquals("(5,5)", explorer.getSize());
+		String size = explorer.getSize();
+		
+		assertEquals("(100,100)", size);
+	}
+	
+	@Test
+	public void test_landExplorer_Initial_Position_0_0_F() {
+		PlanetExplorer explorer = new PlanetExplorer(100, 100, null);
+		
+		String result = explorer.executeCommand("");
+		
+		assertEquals("(0,0,N)", result);
 	}
 }
