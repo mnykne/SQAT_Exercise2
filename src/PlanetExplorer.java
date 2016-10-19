b3af4c3a1c089243eaf816f9bd5e9de1be096739
somename <someemail>
2016-10-19 15:47:46 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -36,14 +36,26 @@ public class PlanetExplorer {
 		 
 	 */
 		for (int i = 0; i < obstacles.length(); i++) {
+			String coordX = -1;
+			String coordY = -1;
 			String strX = "";
-			for (int j = i; j < obstacles.length(); j++) {
-				if (obstacles.charAt(j) == ')') {
-					break;
+			String strY = "";
+			
+			if (obstacles.charAt(i) == '(') {
+				++i;
+				for (int j = i; j < obstacles.length(); j++) {
+					if (obstacles.charAt(j) == ')') {
+						i = j;
+						break;
+					}
+					if (obstacles.charAt(j) >= '0' && obstacles.charAt(j) <= '9') {
+						strX += obstacles.charAt(j);
+					}
 				}
-				if (obstacles.charAt(j) >= '0' && obstacles.charAt(j) <= '9') {
-					strX += obstacles.charAt(j);
-				}
+			
+			
+			} else {
+				throw new PlanetExplorerException();
 			}
 		}
 		
