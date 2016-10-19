2630930ae5badc234f6a71cdd9dba47851814117
somename <someemail>
2016-10-19 16:12:56 +0300

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -47,6 +47,7 @@ public class PlanetExplorer {
 				m2.find();
 				int cx = Integer.parseInt(m2.group(1));
 				int cy = Integer.parseInt(m2.group(2));
+				System.out.println(cx + "," + cy);
 				if (cx < 0 || cx >= mWidth) {
 					throw new PlanetExplorerException();
 				}
