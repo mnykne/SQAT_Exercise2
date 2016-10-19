db6daf5c5289187b77273db73cd701cbcd0bc45b
somename <someemail>
2016-10-19 14:51:58 +0300

    besouro automatic message

--- tests/TestPlanetExplorer.java
+++ tests/TestPlanetExplorer.java
@@ -29,7 +29,7 @@ public class TestPlanetExplorer {
 	}
 	
 	@Test
-	public void test_landExplorer_TurnRight_Facing_East() {
+	public void test_landExplorer_Turn_Right_Facing_East() {
 		PlanetExplorer explorer = new PlanetExplorer(100, 100, null);
 		
 		String result = explorer.executeCommand("r");
